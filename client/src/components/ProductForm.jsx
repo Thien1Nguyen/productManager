0ad5136f09ff483100a236004847c9507f7d0b35
Thien1Nguyen <tnguyen5219@gmail.com>
2023-05-13 23:42:30 -0700

    PM (part II)+

--- client/src/components/ProductForm.jsx
+++ client/src/components/ProductForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -7,6 +7,8 @@ const ProductForm = () => {
     const [price, setPrice] = useState("");
     const [description, setDescription] = useState("");
     const [loaded, setLoaded] = useState(false);
+
+    const [refresh, setRefresh] = useState("")
 
     const [productsList, setProductsList] = useState([])
 
@@ -23,52 +25,54 @@ const ProductForm = () => {
 
     useEffect(() => {
         axios.get(`http://localhost:8000/api/products`)
-        .then((response) => {
-            setProductsList(response.data.results)
-            setLoaded(true); 
-        })
-        .catch((err) => {
-            console.log("Them Errors: ", err)
-        })
-    }, []);
+            .then((response) => {
+                setProductsList(response.data.results)
+                setLoaded(true);
+            })
+            .catch((err) => {
+                console.log("Them Errors: ", err)
+            })
+    }, [refresh]);
 
-    
+
 
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        
-        axios.post('http://localhost:8000/api/products/new',{
+
+        axios.post('http://localhost:8000/api/products/new', {
             title,
             price,
             description
         })
-        .then((res => console.log(res)))
-        .catch(err => console.log(err))
+            .then((res => setRefresh(res)))
+            .catch(err => console.log(err))
 
 
         setTitle("");
         setPrice("");
         setDescription("");
+        setRefresh(!refresh)
     }
+
 
     return (
         <div className='container'>
             <h1 className='text-center mb-5'>Product Manager</h1>
             <div className='d-flex justify-content-center'>
-                <form style={{width: 300}} onSubmit={handleSubmit}>
+                <form style={{ width: 300 }} onSubmit={handleSubmit}>
                     <p className='text-bg-secondary p-2 d-flex justify-content-between'>
                         <label className='form-label me-2'>Title: </label>
-                        <input className='form-control-sm' type="text" onChange={(e)=>setTitle(e.target.value)} value={title}/>
+                        <input className='form-control-sm' type="text" onChange={(e) => setTitle(e.target.value)} value={title} />
                     </p>
                     <p className='text-bg-secondary p-2 d-flex justify-content-between'>
                         <label className='form-label me-2'>Price: </label>
-                        <input className='form-control-sm' type="number" onChange={(e) =>setPrice(parseInt(e.target.value))} value={price}/>
+                        <input className='form-control-sm' type="number" onChange={(e) => setPrice(parseInt(e.target.value))} value={price} />
                     </p>
                     <p className='text-bg-secondary p-2 d-flex justify-content-between'>
                         <label className='form-label me-2'>Description: </label>
-                        <input className='form-control-sm' type="text" onChange={(e) =>setDescription(e.target.value)} value={description}/>
+                        <input className='form-control-sm' type="text" onChange={(e) => setDescription(e.target.value)} value={description} />
                     </p>
                     <button className='mt-3 btn btn-danger btn-center'>Create</button>
                 </form>
@@ -77,11 +81,11 @@ const ProductForm = () => {
             <div className='container'>
                 <h1>Thien's Store</h1>
                 {
-                    productsList.map((item, i) =>{
-                        return(
-                        <div>
-                            <Link key={i} to={`/${item._id}`}>{item.title}</Link> 
-                        </div>
+                    productsList.map((item, i) => {
+                        return (
+                            <div>
+                                <Link key={i} to={`/${item._id}`}>{item.title}</Link>
+                            </div>
                         )
                     })
                 }
